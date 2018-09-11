2740e566880bacd9a1121b744736a77c1818a681
Avedis Balian <avedisbalian@Avediss-MacBook-Pro.local>
2018-09-11 19:55:11 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,39 @@
+alert ("this homework is related to rap")
+alert ("I hope you know everyone that I am talking about")
+alert ("If you do, you are a legend")
+alert ("if you know calculus, help please?")
+const firstName= 'Lil';
+const lastName= 'pump';
+const age= 18;
+if (age<=18 || firstName.length===3){
+	console.log ("Eminem does not like you");
+	if (age<=18)
+		console.log (`Eminem likes you but just do your homework and study, don't do drugs`);
+
+} else {
+	console.log ("still, do your homework");
+}
+if (firstName==="machine gun kelly") {
+	console.log ("you are irrelevant");
+
+} else {
+	console.log ("don't make a disstrack on Eminem");
+}
+
+const hw1= 95;
+const hw2= 90;
+const hw3= 91;
+const hw4= 85;
+const hw5= 84;
+const quiz1= 80;
+const quiz2= 90;
+const quiz3= 95;
+const quiz4= 80;
+const quiz5= 85;
+const hwAverage= hw1+hw2+hw3+hw4+hw5/5
+const quizAverage= quiz1+quiz2+quiz3+quiz4+quiz5/5
+if (hwAverage > 70 && quizAverage >= 86){
+	console.log (` B, pretty good`)
+} else {
+	console.log ("study harder next time")
+}
